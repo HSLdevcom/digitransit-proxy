ab5249d28c8aab5bae21cd17118c5167a73c0c6f
Joel Lappalainen <lappalj8@gmail.com>
2024-08-27 17:02:44 +0300

    Remove legacy hack

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/ash
 
 set -e
-#workaround for azure DNS issue
-if [ -n "$MESOS_CONTAINER_NAME"  ]; then 
-  echo "search marathon.l4lb.thisdcos.directory" >> /etc/resolv.conf;
-fi
+
 sed -i "s/VILKKU_BASIC_AUTH/${VILKKU_BASIC_AUTH}/" /etc/nginx/external.conf
 sed -i "s/JOJO_BASIC_AUTH/${JOJO_BASIC_AUTH}/" /etc/nginx/external.conf
 sed -i "s/LAPPEENRANTA_BASIC_AUTH/${LAPPEENRANTA_BASIC_AUTH}/" /etc/nginx/external.conf
